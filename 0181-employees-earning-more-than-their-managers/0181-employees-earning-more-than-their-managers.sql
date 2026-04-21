747a653ddbf361c7a3b176219504660d1ebd2443
HOSEON YOO <hsyoodev@icloud.com>
2026-04-21 23:53:44 +0900

    Time: 573 ms (12.92%), Space: 0B (100%) - LeetHub

---
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -0,0 +1,11 @@
+/* Write your PL/SQL query statement below */
+SELECT
+        A.name AS Employee
+FROM
+        Employee A
+INNER JOIN
+        Employee B
+ON
+        A.managerId = B.id
+AND
+        A.salary > B.salary;
