4eee61a3d5309df8660dc434c6cbc3209fd81f9c
HOSEON YOO <hsyoodev@icloud.com>
2026-04-29 21:46:49 +0900

    Time: 723 ms (31.75%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,32 +1,20 @@
 /* Write your PL/SQL query statement below */
-WITH
-    Custom AS
-    (
-        SELECT
-                A.name AS Employee
-              , B.name AS Department
-              , A.departmentId
-              , A.salary
-        FROM
-                Employee A
-        INNER JOIN 
-                Department B
-        ON
-                A.departmentId = B.id
-    )
 SELECT
-        Department
-      , Employee
-      , Salary
+        B.name AS Department
+      , A.name AS Employee
+      , A.salary AS Salary
 FROM
-        CUSTOM
-WHERE
-        (departmentId, salary) IN (
-                                    SELECT
-                                            departmentId
-                                          , MAX(salary)
-                                    FROM
-                                            CUSTOM
-                                    GROUP BY
-                                            departmentId
-                                  );
+        Employee A
+INNER JOIN 
+        Department B
+ON
+        A.departmentId = B.id
+AND
+        A.salary IN (
+                        SELECT
+                                MAX(salary)
+                        FROM
+                                Employee
+                        WHERE
+                                departmentId = A.departmentId
+                    );
