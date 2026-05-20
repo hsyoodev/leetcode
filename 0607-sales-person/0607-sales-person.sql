3301a9b6a616ac8fee2717cabdf1f03d3fb77678
HOSEON YOO <hsyoodev@icloud.com>
2026-05-20 20:46:36 +0900

    Time: 1492 ms (14.66%), Space: 0B (100%) - LeetHub

---
+++ 0607-sales-person/0607-sales-person.sql
@@ -0,0 +1,19 @@
+/* Write your PL/SQL query statement below */
+SELECT
+        name
+FROM
+        SalesPerson
+WHERE
+        sales_id NOT IN 
+        (
+            SELECT
+                    Orders.sales_id
+            FROM
+                    Orders
+            INNER JOIN
+                    Company
+            ON
+                    Orders.com_id = Company.com_id
+            WHERE
+                    Company.name = 'RED'
+        );
