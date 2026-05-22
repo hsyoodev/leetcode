911d819658181877fc49aaf58330195170086c42
HOSEON YOO <hsyoodev@icloud.com>
2026-05-22 20:32:00 +0900

    Time: 412 ms (31.56%), Space: 0B (100%) - LeetHub

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,13 @@
+/* Write your PL/SQL query statement below */
+SELECT
+        x
+      , y
+      , z
+      , (
+            CASE 
+                WHEN ((x + y) <= z) OR ((x+ z) <= y) OR ((y + z) <= x) THEN 'No'
+                ELSE 'Yes'
+            END
+        ) AS triangle
+FROM
+        Triangle;
