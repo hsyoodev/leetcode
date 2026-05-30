ce65e13f7f72c7934c32fa98624a95d649cb01df
HOSEON YOO <hsyoodev@icloud.com>
2026-05-30 14:04:34 +0900

    Time: 16 ms (28.48%), Space: 46.2 MB (51.69%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.kt
+++ 0013-roman-to-integer/0013-roman-to-integer.kt
@@ -1,30 +1,31 @@
 class Solution {
     fun romanToInt(s: String): Int {
-        val map = mapOf<String, Int>(
-            "I" to 1,
-            "V" to 5,
-            "X" to 10,
-            "L" to 50,
-            "C" to 100,
-            "D" to 500,
-            "M" to 1000,
-            "IV" to -2,
-            "IX" to -2,
-            "XL" to -20,
-            "XC" to -20,
-            "CD" to -200,
-            "CM" to -200
+        val map = mapOf<Char, Int>(
+            'I' to 1,
+            'V' to 5,
+            'X' to 10,
+            'L' to 50,
+            'C' to 100,
+            'D' to 500,
+            'M' to 1000
         )
-        val arr = ArrayDeque<String>()
         var answer = 0;
 
-        s.forEach { ch -> 
-            answer += map.getValue(ch.toString())
-            arr.add(ch.toString())
+        for(index in s.indices) {
+            val currentKey = s[index]
+            val currentValue = map.getValue(currentKey)
             
-            if(arr.count() == 2) {
-                answer += map.getOrDefault(arr.joinToString(""), 0)
-                arr.removeFirst()
+            if(index + 1 < s.length) {
+                val nextKey = s[index + 1]
+                val nextValue = map.getValue(nextKey)
+
+                if(currentValue >= nextValue) {
+                    answer += currentValue
+                } else {
+                    answer -= currentValue
+                } 
+            } else {
+                answer += currentValue
             }
         }    
 
