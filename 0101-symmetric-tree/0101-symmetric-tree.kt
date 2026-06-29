e0e107d572f6d7842de780b0815f042b05146427
HOSEON YOO <hsyoodev@icloud.com>
2026-06-29 23:10:01 +0900

    Time: 0 ms (100%), Space: 41.9 MB (46.56%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.kt
+++ 0101-symmetric-tree/0101-symmetric-tree.kt
@@ -9,21 +9,19 @@
  * }
  */
 class Solution {
-    val bfs1: MutableList<Int?> = mutableListOf()
-    val bfs2: MutableList<Int?> = mutableListOf()
-
     fun isSymmetric(root: TreeNode?): Boolean {
-        return isSame(root?.left, root?.right)
+        return isMirror(root?.left, root?.right)
     }
 
-    fun isSame(left: TreeNode?, right: TreeNode?): Boolean {
+    fun isMirror(left: TreeNode?, right: TreeNode?): Boolean {
         if (left == null && right == null) {
             return true
         }
+
         if (left == null || right == null) {
             return false
         }
-        return left.`val` == right.`val` && isSame(left.left, right.right) && isSame(right.left, left.right)
+
+        return left.`val` == right.`val` && isMirror(left.left, right.right) && isMirror(right.left, left.right)
     }
  }
- 
