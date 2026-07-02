3faa4b856b029a5276146b4b33560826aa0ef49c
HOSEON YOO <hsyoodev@icloud.com>
2026-07-03 00:08:07 +0900

    Time: 6 ms (64.23%), Space: 67.1 MB (63.41%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.kt
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.kt
@@ -14,12 +14,13 @@ class Solution {
             return 0
         }
 
-        if(root.left == null) {
-            return 1 + minDepth(root.right)
-        } else if(root.right == null) {
-            return 1 + minDepth(root.left)
-        }
+        val left = minDepth(root.left)
+        val right = minDepth(root.right)
 
-        return 1 + Math.min(minDepth(root.left), minDepth(root.right))
+        return when {
+            root.left == null   -> 1 + right
+            root.right == null  -> 1 + left
+            else                -> 1 + minOf(left, right) 
+        }
     }
 }
