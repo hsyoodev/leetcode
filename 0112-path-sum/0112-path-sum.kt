05e48843211d7875081fec97b2b21e5746a725bc
HOSEON YOO <hsyoodev@icloud.com>
2026-07-04 12:00:11 +0900

    Time: 0 ms (100%), Space: 42.9 MB (76.44%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.kt
@@ -0,0 +1,29 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun hasPathSum(root: TreeNode?, targetSum: Int): Boolean {
+        if(root != null) {
+            if(root.left == null && root.right == null) {
+                if((targetSum - root.`val`) == 0) {
+                    return true
+                } else {
+                    return false
+                }
+            } 
+        }
+
+        if(root == null) {
+            return false
+        }
+
+        return hasPathSum(root.left, targetSum - root.`val`) || hasPathSum(root.right,targetSum - root.`val`)
+    }
+}
