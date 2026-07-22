72ccb1716a25b5937cf82c3acbc321c709371bd5
HOSEON YOO <hsyoodev@icloud.com>
2026-07-22 19:55:34 +0900

    Time: 364 ms (41.7%), Space: 0B (100%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+SELECT
+        E1.employee_id
+FROM
+        Employees E1
+LEFT OUTER JOIN
+        Employees E2
+ON
+        E1.manager_id = E2.employee_id
+WHERE
+        E1.salary < 30000
+AND
+        E2.employee_id IS NULL
+AND
+        E1.manager_id IS NOT NULL
+ORDER BY
+        E1.employee_id;
