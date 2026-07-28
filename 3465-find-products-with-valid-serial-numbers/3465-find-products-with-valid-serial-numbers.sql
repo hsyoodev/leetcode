0fe897b23713c43851e73d48b486a3d002e16d9c
HOSEON YOO <hsyoodev@icloud.com>
2026-07-28 22:08:00 +0900

    Time: 522 ms (23.17%), Space: 0B (100%) - LeetHub

--- 3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
+++ 3465-find-products-with-valid-serial-numbers/3465-find-products-with-valid-serial-numbers.sql
@@ -4,6 +4,6 @@ SELECT
 FROM
         products
 WHERE
-        REGEXP_LIKE(description, '(( SN|^SN)[0-9]{4}-([0-9]{4}$|[0-9]{4} ))', 'c')
+        REGEXP_LIKE(description, '\\bSN[0-9]{4}-[0-9]{4}\\b', 'c')
 ORDER BY
         product_id;
