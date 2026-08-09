d8ec01ea4f574c065a86cd132cf197654d7e4f9a
HOSEON YOO <hsyoodev@icloud.com>
2026-08-09 12:36:51 +0900

    Time: 0 ms (100%), Space: 40.9 MB (35.29%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.kt
+++ 0231-power-of-two/0231-power-of-two.kt
@@ -1,16 +1,5 @@
 class Solution {
     fun isPowerOfTwo(n: Int): Boolean {
-        var powerOfTwo = n
-        var countLastOneBits = 0
-
-        while(powerOfTwo > 0) {
-            if((powerOfTwo and 1) == 1) {
-                countLastOneBits++
-            }
-
-            powerOfTwo = powerOfTwo shr 1
-        }
-
-        return (countLastOneBits == 1) 
+        return (n > 0) && ((n and (n - 1)) == 0)
     }
 }
