36509265e07bf1adbb1e67b429d8d9a4e52146b9
HOSEON YOO <hsyoodev@icloud.com>
2026-08-17 13:58:15 +0900

    Time: 479 ms (12.33%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,25 +1,12 @@
 # Write your MySQL query statement below
-(
-    SELECT
-            CASE
-                WHEN id IN (SELECT MAX(id) FROM Seat) THEN id
-                ELSE id + 1
-            END AS id
-        , student
-    FROM
-            Seat
-    WHERE
-            MOD(id, 2) = 1
-)
-UNION ALL
-(
-    SELECT
-            (id - 1) AS id
-          , student
-    FROM
-            Seat
-    WHERE
-            MOD(id, 2) = 0
-)
+SELECT
+        CASE
+            WHEN id % 2 = 1 AND id NOT IN (SELECT MAX(id) FROM Seat) THEN id + 1
+            WHEN id % 2 = 0 THEN id - 1
+            ELSE id
+        END AS id
+      , student
+FROM
+        Seat
 ORDER BY
         id;
