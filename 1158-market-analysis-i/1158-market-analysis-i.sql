4c580b8b6ace1a92e880ac6e51d7b650ed25564e
HOSEON YOO <hsyoodev@icloud.com>
2026-08-25 23:14:53 +0900

    Time: 1263 ms (75.84%), Space: 0B (100%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -2,16 +2,14 @@
 SELECT
         U.user_id AS buyer_id
       , U.join_date
-      , COUNT(
-                CASE
-                    WHEN YEAR(O.order_date) = '2019' THEN 1
-                END
-             ) AS orders_in_2019
+      , COUNT(O.order_date) AS orders_in_2019
 FROM
         Users U
 LEFT OUTER JOIN
         Orders O
 ON 
         U.user_id = O.buyer_id
+AND
+        YEAR(O.order_date) = '2019'
 GROUP BY
-        U.user_id, U.join_date;
+        U.user_id;
