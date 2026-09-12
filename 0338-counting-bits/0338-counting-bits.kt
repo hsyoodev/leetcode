3110b9eb76a24a96f63bcefde5ac45ca6fd9307c
HOSEON YOO <hsyoodev@icloud.com>
2026-09-12 19:59:30 +0900

    Time: 2 ms (85.32%), Space: 51.5 MB (35.78%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.kt
+++ 0338-counting-bits/0338-counting-bits.kt
@@ -3,21 +3,7 @@ class Solution {
         val answer = IntArray(n + 1)
 
         for(i in 0..n) {
-            var countOneBits = 0
-            var num = i
-            while(num >= 2) {
-                if(num % 2 == 1) {
-                    countOneBits++
-                }
-
-                num /= 2
-            }
-
-            if(num == 1) {
-                countOneBits++
-            }
-
-            answer[i] = countOneBits
+            answer[i] = answer[i shr 1] + (i and 1)
         }
 
         return answer
